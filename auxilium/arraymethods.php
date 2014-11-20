bc5f36820448f85688c25fe42a68c31fede6fd83
Aleksandar Popovic <web.development.serbia@gmail.com>
2014-11-20 19:25:21 +0100

    ArrayMethods class

--- auxilium/arraymethods.php
+++ auxilium/arraymethods.php
@@ -2,10 +2,36 @@
 
 namespace Auxilium {
     
-    class AuxiliumArrayMethods {
+    class ArrayMethods {
         
+        private function __construct() {
+            
+        }
         
+        private function __clone() {
+            
+        }
+    
+        public static function clean($array) {
+            
+            return array_filter($array, function($item) {
+               
+                return !empty($item);
+                
+            });
+            
+        }
         
+        public static function trim($array) {
+            
+            return array_map(function($item) {
+                
+                return trim($item);
+                
+            }, $array);
+            
+        }
+    
     }
     
 }
